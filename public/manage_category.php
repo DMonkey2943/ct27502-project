83c819236432a8bbefca5cf2e886072cc8911129
DMonkey2943 <lntduyen29042003@gmail.com>
2024-03-13 22:42:31 +0700

    build manage_category.php

---
+++ public/manage_category.php
@@ -0,0 +1,62 @@
+<?php include_once __DIR__ . '/../src/partials/header.php' ?>
+
+<body>
+    <?php include_once __DIR__ . '/../src/partials/navbar.php' ?>
+
+    <div class="container">
+        <?php
+        $subtitle = 'QUẢN LÝ DANH MỤC';
+        include_once __DIR__ . '/../src/partials/heading.php';
+        ?>
+
+        <div class="row my-5">
+            <div class="col-md-4">
+                <h2>Thêm danh mục</h2>
+                <form action="" method="POST">
+                    <div>
+                        <label for="cat_name">Tên danh mục</label>
+                        <input type="text" class="form-control fs-5" name="cat_name" id="cat_name"> <br>
+                    </div>
+                    <input type="submit" name="add_cat" value="Thêm danh mục" class="btn btn-primary fs-5">
+                </form>
+            </div>
+
+            <!-- Cập nhật -->
+            <!-- <div class="col-md-4">
+                <h4>Cập nhật danh mục</h4>
+                <form action="" method="POST">
+                    <input type="hidden" class="form-control" name="cat_id" value="5">
+                    <div>
+                        <label for="cat_name">Tên danh mục</label>
+                        <input type="text" class="form-control fs-5" name="cat_name" id="cat_name" value="Tivi"> <br>
+                    </div>
+                    <input type="submit" name="update_cat" value="Cập nhật danh mục" class="btn btn-primary fs-5">
+                </form>
+            </div> -->
+
+            <div class="col-md-8">
+                <h2>Liệt kê danh mục</h2>
+                <table class="table table-bordered">
+                    <tr>
+                        <th>Thứ tự</th>
+                        <th>Tên danh mục</th>
+                        <th>Quản lý</th>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Tivi</td>
+                        <td>
+                            <a href="?action=delete&id=">Xóa</a> ||
+                            <a href="?action=update&id=">Cập nhật</a>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+
+    </div>
+
+    <?php include_once __DIR__ . '/../src/partials/footer.php' ?>
+</body>
+
+</html>
